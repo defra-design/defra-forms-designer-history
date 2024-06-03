268fc90f3dafaa3fae9c9c8ae1f282f27b26860a
daniel-defra <daniel.dasilveira@defra.gov.uk>
2024-06-03 11:20:54 +0100

    theme colour updated to defra branding

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,22 +1,21 @@
 module.exports = function (eleventyConfig) {
   // Options to customise the appearance of your design history
   // https://x-govuk.github.io/govuk-eleventy-plugin/options/
-  eleventyConfig.addPlugin(require('@x-govuk/govuk-eleventy-plugin'), {
-    stylesheets: [
-      '/styles/application.css'
-    ],
+  eleventyConfig.addPlugin(require("@x-govuk/govuk-eleventy-plugin"), {
+    stylesheets: ["/styles/application.css"],
     headingPermalinks: true,
     header: {
       organisationLogo: false,
-      productName: 'Forms designer history',
-      themeColor: '#f7f7f7', // Change this to your desired theme color
+      productName: "Forms Designer - Design History",
       search: {
-        indexPath: '/search.json',
-        sitemapPath: '/sitemap'
+        indexPath: "/search.json",
+        sitemapPath: "/sitemap",
+      },
+      logotype: {
+        text: "Defra", // Example text, replace with your desired text
       }
-    }
-  })
-
+    },
+  });
 
   // Passthrough
   eleventyConfig.addPassthroughCopy({ './app/images': '.' })
